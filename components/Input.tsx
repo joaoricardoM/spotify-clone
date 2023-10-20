0f78e287a5cb13cf082a8f9568a5c28ee617f522
joaoricardoM <joao_pia_mds@hotmail.com>
2023-10-20 16:18:48 -0300

    feat: create modal to your library

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,12 +1,41 @@
-'use client'
-
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
 import React, { forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, disabled, ...prop }, ref) => {
-    return <input />
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          `
+        flex
+        w-full
+        rounded-md
+        bg-neutral-700
+        border
+        border-transparent
+        px-3
+        py-3
+        text-sm
+        file:border-0
+        file:border-transparent
+        file:text-sm
+        placeholder:text-neutral-400
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        focus:outline-none
+      `,
+          disabled && 'opacity-75',
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    )
   }
 )
 
