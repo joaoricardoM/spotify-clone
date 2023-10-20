54308c3d43d2e4ac29198c023900723ea33d1afc
joaoricardoM <joao_pia_mds@hotmail.com>
2023-10-20 17:00:04 -0300

    feat: create list songs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['ckeaqzcqkqjcrbnhcbnl.supabase.co']
+  }
+}
 
 module.exports = nextConfig
