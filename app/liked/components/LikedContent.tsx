badaf9ceae7a28463b8236589d3b657e539fa39f
joaoricardoM <joao_pia_mds@hotmail.com>
2023-10-26 12:30:07 -0300

    feat: functionality

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -7,6 +7,7 @@ import { Song } from '@/types'
 import { useUser } from '@/hooks/useUser'
 import { MediaItem } from '@/components/MediaItem'
 import { LikeButton } from '@/components/LikeButton'
+import useOnPlay from '@/hooks/useOnPlay'
 
 interface LikedContentProps {
   songs: Song[]
@@ -16,7 +17,7 @@ export const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
   const router = useRouter()
   const { isLoading, user } = useUser()
 
-  // const onPlay = useOnPlay(songs)
+  const onPlay = useOnPlay(songs)
 
   useEffect(() => {
     if (!isLoading && !user) {
@@ -37,8 +38,7 @@ export const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       {songs.map((song: any) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
-            {/* onClick={(id) => onPlay(id)} data={song} */}
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -2,6 +2,7 @@
 
 import { LikeButton } from '@/components/LikeButton'
 import { MediaItem } from '@/components/MediaItem'
+import useOnPlay from '@/hooks/useOnPlay'
 import { Song } from '@/types'
 import React from 'react'
 
@@ -10,6 +11,7 @@ interface SearchContentProps {
 }
 
 export const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs)
   if (songs.length === 0) {
     return (
       <div
@@ -40,12 +42,9 @@ export const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-centergap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem
-              onClick={() => {}} //(id: string) => onPlay(id)
-              data={song}
-            />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
-          <LikeButton songId={song.id}/>
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
