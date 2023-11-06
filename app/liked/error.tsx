a79c0d87c26d08a9de5dd645855ce4bb08371901
joaoricardoM <joao_pia_mds@hotmail.com>
2023-11-06 12:03:04 -0300

    .

--- app/liked/error.tsx
+++ app/liked/error.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Error = () => {
   return (
     <Box className="h-full flex items-center justify-center">
-      <div className="text-neutral-400">Somenthing went wrong</div>
+      <div className="text-neutral-400">Error</div>
     </Box>
   )
 }
