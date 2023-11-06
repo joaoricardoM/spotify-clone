8da95e58ae58170244582e21070e0d1513d19bfc
joaoricardoM <joao_pia_mds@hotmail.com>
2023-11-06 12:19:34 -0300

    .

--- next.config.js
+++ next.config.js
@@ -3,14 +3,11 @@ const path = require('path')
 const nextConfig = {
   images: {
     domains: ['ckeaqzcqkqjcrbnhcbnl.supabase.co']
-  }
-}
-
-module.exports = {
-  // ...
-  nextConfig,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname)
     return config
   }
 }
+
+module.exports = nextConfig
